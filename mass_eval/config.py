cafa79e788a2a7d063b36b5046b86ab54a98b39c
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-07-07 19:56:08 +0200

    Remove mushra.yaml from package and provide it as an example

--- mass_eval/config.py
+++ mass_eval/config.py
@@ -6,12 +6,9 @@ dsd_yaml = pkg_resources.resource_filename('mass_eval',
 mus_csv = pkg_resources.resource_filename('mass_eval',
                                           'data/sisec_mus_2017_full.csv')
 
-mushra_yaml = pkg_resources.resource_filename('mass_eval',
-                                              'data/mushra.yaml')
-
 # User configurable:
 mus_base_path = None
 dsd_base_path = None
+mushra_config_file = None
 fs = 44100
 audio_encoding = 'float32'
-mushra_config_file = mushra_yaml
--- mass_eval/mushra.py
+++ mass_eval/mushra.py
@@ -135,38 +135,3 @@ def mushra_mixture_from_track_sample(sample,
 
         with open(filename, 'w') as file:
             file.write(filedata)
-
-
-if __name__ == '__main__':
-
-    import mass_eval
-    import pandas as pd
-    import numpy as np
-
-    mass_eval.config.mus_base_path = '/vol/vssp/maruss/data2/MUS2017'
-    mass_eval.config.dsd_base_path = '/vol/vssp/datasets/audio/DSD100'
-
-    df = mass_eval.data.get_sisec_df()
-
-    exclude_tracks = []
-    mix_sample = pd.DataFrame()
-    for metric in ['SDR', 'SAR', 'SIR']:
-
-        sample = mass_eval.data.get_sample(
-                df,
-                num_tracks=2,
-                num_algos=4,
-                metric=metric,
-                target='vocals',
-                only_these_algos=None,
-                exclude_tracks=exclude_tracks,
-                selection_plot=False)
-
-        tracks = sample['track_id'].values
-        exclude_tracks = np.append(exclude_tracks, np.unique(tracks))
-        mix_sample = pd.concat([mix_sample, sample])
-
-    mushra_mixture_from_track_sample(mix_sample,
-                                     '.',
-                                     target_loudness=-26,
-                                     mixing_levels=[0, 6, 12])
